54605056e088750f67b7fb737f35b132bed9899a
Runyasak Chaengnaimuang <world@odds.team>
2023-06-14 23:10:10 +0700

    add sidebar-list

--- apps/demo-app/src/app/layouts/example-layout/example-layout.component.ts
+++ apps/demo-app/src/app/layouts/example-layout/example-layout.component.ts
@@ -1,10 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterOutlet,
+} from '@angular/router';
+import { SfListItemComponent } from '@ng-storefront-ui/lib';
+import { filter, map } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SfListItemComponent, RouterLink],
   templateUrl: './example-layout.component.html',
   styles: [
     `
@@ -15,4 +23,24 @@ import { RouterOutlet } from '@angular/router';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExampleLayoutComponent {}
+export class ExampleLayoutComponent {
+  components = [
+    { name: 'Button', url: '/sf-button' },
+    { name: 'List Item', url: '/sf-list-item' },
+  ];
+
+  navigationUrl$ = this.router.events.pipe(
+    map((event) => {
+      if (event instanceof NavigationEnd) {
+        return event.url;
+      }
+
+      return undefined;
+    }),
+    filter((url) => !!url)
+  );
+
+  currentUrl = toSignal(this.navigationUrl$);
+
+  constructor(private router: Router) {}
+}
