8f3db1bc73d519161ecba73299b79bf2aadd9c44
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-07-02 23:43:44 +0700

    rearrange public-api

--- packages/ng-storefront-ui/components/public-api.ts
+++ packages/ng-storefront-ui/components/public-api.ts
@@ -2,10 +2,10 @@
  * Public API Surface of ng-storefront-ui
  */
 
-export * from './sf-icon-base/sf-icon-base.component';
-
 export * from './sf-button/sf-button.component';
 
 export * from './sf-counter/sf-counter.component';
+
+export * from './sf-icon-base/sf-icon-base.component';
 
 export * from './sf-list-item/sf-list-item.component';
