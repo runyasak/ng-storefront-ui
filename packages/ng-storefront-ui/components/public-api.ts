caced91f8eb614dfe92d56e65e34ee0bc79d0c00
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-07-03 00:00:47 +0700

    feat(sf-icon-tune): new component

--- packages/ng-storefront-ui/components/public-api.ts
+++ packages/ng-storefront-ui/components/public-api.ts
@@ -8,4 +8,6 @@ export * from './sf-counter/sf-counter.component';
 
 export * from './sf-icon-base/sf-icon-base.component';
 
+export * from './sf-icon-tune/sf-icon-tune.component';
+
 export * from './sf-list-item/sf-list-item.component';
