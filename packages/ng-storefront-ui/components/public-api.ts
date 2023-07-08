b4538234549d23d5fabf57206d069633e46c583e
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-07-08 23:16:33 +0700

    feat(sf-icon-shopping-cart): new component

--- packages/ng-storefront-ui/components/public-api.ts
+++ packages/ng-storefront-ui/components/public-api.ts
@@ -10,6 +10,8 @@ export * from './sf-icon-base/sf-icon-base.component';
 
 export * from './sf-icon-chevron-right/sf-icon-chevron-right.component';
 
+export * from './sf-icon-shopping-cart/sf-icon-shopping-cart.component';
+
 export * from './sf-icon-tune/sf-icon-tune.component';
 
 export * from './sf-list-item/sf-list-item.component';
