261b66c882f107458af5aea341565beda9faca88
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-09-05 23:16:16 +0700

    update index

--- projects/ng-storefront-ui/index.ts
+++ projects/ng-storefront-ui/index.ts
@@ -4,6 +4,12 @@ export * from './src/lib/sf-icons';
 
 export * from './src/lib/tailwind.config';
 
+export * from './src/lib/sf-badge/sf-badge.component';
+
 export * from './src/lib/sf-button/sf-button.component';
 
+export * from './src/lib/sf-counter/sf-counter.component';
+
 export * from './src/lib/sf-list-item/sf-list-item.component';
+
+export * from './src/lib/sf-rating/sf-rating.component';
