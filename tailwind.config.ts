5dab9e09bfa2411dee40c49065a7566bdc2170e0
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-09-09 21:22:49 +0700

    demo: add storefront-ui typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,16 @@
 import { tailwindConfig } from './projects/ng-storefront-ui/src/lib/tailwind.config';
 import { Config } from 'tailwindcss';
+import sfTypography from '@storefront-ui/typography';
 
 export default <Config>{
   presets: [tailwindConfig],
   content: ['./projects/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Red Hat Text, sans-serif',
+      },
+    },
+  },
+  plugins: [sfTypography],
 };
